3f6553622637939c294fe5da1fc3e5a99f328088
hlool24 <halahhelabi@gmail.com>
2022-10-02 15:29:00 +0300

    create API for file metaData

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer= require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'),(req,res) => {
+  const fileInfo= req.file;
+  
+  
+  res.status(200).json({"name":decodeURIComponent(escape( fileInfo.originalname)),"type":fileInfo.mimetype,"size":fileInfo.size})
+})
 
 
 const port = process.env.PORT || 3000;
